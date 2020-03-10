d788864a319bb903a923365295358ca389344561
oatw <oatwoatw@gmail.com>
2020-03-10 12:19:01 +0800

    Updates bubble sort.

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,26 +1,21 @@
 class Array
 
-  # time: O(n²) 
-  # space: O(1)
+  # T(n) = O(n²)
+  # S(n) = O(1)
   # stable: true
   def bubble_sort!(order = :asc)
     return self if empty?
-    sort_times_limit = sort_border_index = size - 1
-    last_exchange_index = sort_times = 0
-    while sort_times < sort_times_limit
+    max_sort_times = sort_border = size - 1
+    max_sort_times.times.each do
       is_sorted = true
-      sort_index = 0
-      while sort_index < sort_border_index  
-        if self[sort_index].send(order == :asc ? :> : :<, self[sort_index + 1])
-          self[sort_index], self[sort_index + 1] = self[sort_index + 1], self[sort_index]
+      (0...sort_border).each do |i|
+        if self[i].send(order === :asc ? :> : :<, self[i + 1])
+          self[i], self[i + 1] = self[i + 1], self[i]
           is_sorted = false
-          last_exchange_index = sort_index
+          sort_border = i
         end
-        sort_index += 1
       end
       break if is_sorted
-      sort_border_index = last_exchange_index
-      sort_times += 1
     end
     self
   end
